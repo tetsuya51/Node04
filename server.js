e31f5f614e5c85cbfbc7d6e0cd4ffb5ced1671e7
christophe Dubois <dubois51ch@gmail.com>
2023-04-25 11:42:52 +0200

    exercice Node 4

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+
 console.log(`The server will run on port ${process.env.SERVER_PORT}`);
-console.log(`The secret key ${process.env.SECRET_API_KEY} will be used to connect to the third party API`);
-console.log(`The server will connect to the database with the user "${process.env.DB_USER}" and the password "${process.env.DB_PASSWORD}"`);
+
+console.log(`I am ${process.env.MY_NAME}, wilder in ${process.env.MY_CITY}, and I love ${process.env.MY_LANGUAGE}.`)
